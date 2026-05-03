d7cc218b630c6b51b94d235d3dfd1609a81829f2
Daniel Martins <dragomaia@protonmail.ch>
2026-05-03 22:49:31 +0100

    alterei config para github pages

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,9 +3,9 @@ export const CONFIG = {
   // Site Settings
   // ---------------------------------------------------------------------------
   site: {
-    url: "https://alexmercer.dev",
+    url: "https://danielmartinsulht.github.io",
     locale: "en_US",
-    twitterHandle: "@alexmercer_dev",
+    //twitterHandle: "@alexmercer_dev",
   },
 
   // ---------------------------------------------------------------------------
